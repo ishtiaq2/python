730b77775b40d90a625774c1ba9d7dca2f0f3658
ishtiaq2 <ishtiaq2@kth.se>
2020-03-30 23:02:20 +0200

    tic tac toe

--- udemy/tic_tac_toe.py
+++ udemy/tic_tac_toe.py
@@ -2,7 +2,7 @@ import sys
 
 player1 = ''
 player2 = ''
-
+switch = True
 
 def draw_board():
     print '     |        |      '
@@ -18,17 +18,24 @@ def draw_board():
     print '     |        |      '
 
 def assign_symbols():
-    print 'Player 1: Do you wan to be X or O? \n'
-    player1 = sys.stdin.read(1)
-
-    player2 = 'X' if player1 == 'O' else 'O'
-    print 'Player 1 is: {} and Player 2 is: {}'.format(player1, player2)
+    player1 = raw_input('Player 1: Do you wan to be X or O? ')
+    player2 = 'x' if player1 == 'o' else 'o'
+    print ('Player 1 is: {} and Player 2 is: {}').format(player1, player2)
+    print ('Player 1 will go first.')
+    st = raw_input("Are you ready to play? Enter y or n.")
+    if str(st) == "y":
+        return True
+    else:
+        return False
 
 
 
 def main():
-    draw_board()
-    assign_symbols()
+    
+    if assign_symbols():
+        draw_board()
+    else:
+        return 0
 
 if __name__ == '__main__':
     sys.exit(main())
