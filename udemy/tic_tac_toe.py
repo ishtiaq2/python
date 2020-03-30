99dc9ddc97a22c75e8cb1fe3d1583c63138d8b55
Muhammad Ishtiaq Hussain <33137234+ishtiaq2@users.noreply.github.com>
2020-03-30 17:49:42 +0200

    Update tic_tac_toe.py

--- udemy/tic_tac_toe.py
+++ udemy/tic_tac_toe.py
@@ -1,1 +1,34 @@
+import sys
 
+player1 = ''
+player2 = ''
+
+
+def draw_board():
+    print '     |        |      '
+    print '     |        |      '
+    print '     |        |      '
+    print '-----------------------'
+    print '     |        |      '
+    print '     |        |      '
+    print '     |        |      '
+    print '-----------------------'
+    print '     |        |      '
+    print '     |        |      '
+    print '     |        |      '
+
+def assign_symbols():
+    print 'Player 1: Do you wan to be X or O? \n'
+    player1 = sys.stdin.read(1)
+
+    player2 = 'X' if player1 == 'O' else 'O'
+    print 'Player 1 is: {} and Player 2 is: {}'.format(player1, player2)
+
+
+
+def main():
+    draw_board()
+    assign_symbols()
+
+if __name__ == '__main__':
+    sys.exit(main())
